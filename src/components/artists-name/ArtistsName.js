5047aa0c04f4a03e6fa0d49db185afacb7eccce5
NikaGogelia <gogelianika1@gmail.com>
2023-11-15 00:36:31 +0400

    modify artist name component

--- src/components/artists-name/ArtistsName.js
+++ src/components/artists-name/ArtistsName.js
@@ -12,35 +12,14 @@ const textStyles = {
 
 function ArtistsName({ artists, content }) {
   return (
-    <Typography
-      noWrap
-      sx={textStyles}
-      className="artists-name"
-      variant="body1"
-    >
+    <Typography noWrap sx={textStyles} className="artists-name" variant="body1">
       {content === "playlist" ? (
-        content === "track-playlist" ? (
-          artists?.map((artist) => (
-            <Fragment key={artist.id}>
-              <Link
-                to={`/player/artist/${artist.name}`}
-                state={{ data: artist, content: "artist" }}
-              >
-                {artists.indexOf(artist) === artists.length - 1
-                  ? artist?.name
-                  : artist?.name + ","}
-              </Link>
-              &nbsp;
-            </Fragment>
-          ))
-        ) : (
-          <Link
-          // to={`/player/artist/${artists.display_name}`}
-          // state={{ data: artists, content: "artist" }}
-          >
-            {artists?.display_name}
-          </Link>
-        )
+        <Link
+        // to={`/player/artist/${artists.display_name}`}
+        // state={{ data: artists, content: "artist" }}
+        >
+          {artists?.display_name}
+        </Link>
       ) : (
         artists?.map((artist) => (
           <Fragment key={artist.id}>
